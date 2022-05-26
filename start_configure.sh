e258885a87cf8de879b58d881a50309996cbf8db
Kostya251 <57761507+Kostya251@users.noreply.github.com>
2022-05-26 13:46:33 +0500

    Add files via upload

--- start_configure.sh
+++ start_configure.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-archive_name='archive.tar.gz'
+archive_name='config.tar.gz'
 if [ -f $archive_name ];then
 tar -xzvf $archive_name
 else
 echo "archive does not exist"
 fi
 chmod +x do_configure.sh
+chmod +x add_dovecot_user.sh
 bash do_configure.sh
